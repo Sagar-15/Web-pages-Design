ed1b1d2ee7e859ca0cf8f94ac14549cc5f68cd2a
Sagar Kalmundkar <124123700+Sagar-15@users.noreply.github.com>
2023-06-24 14:32:43 +0100

    Add files via upload

---
+++ Web designs/login/script.js
@@ -0,0 +1,9 @@
+document.getElementById("x").onclick = function(){
+    if(document.getElementById("username","password").value !== ""){
+        window.alert("Login SuccessFully")
+    }
+    else{
+        window.alert("CHUTIYA Nav Tak")
+    }
+
+}
