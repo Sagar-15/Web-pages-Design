15e11a9a04c143c826bd54c22491c5b7970d5d41
Sagar Kalmundkar <124123700+Sagar-15@users.noreply.github.com>
2024-02-05 17:28:41 +0530

    Delete Web designs directory

--- Web designs/login/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("x").onclick = function(){
-    if(document.getElementById("username","password").value !== ""){
-        window.alert("Login SuccessFully")
-    }
-    else{
-        window.alert("Invalid")
-    }
-
-}
